310d8c70da30973dabfca308611e5479a48db347
Stefan Dragoi <Stefan.Dragoi@centric.eu>
2024-07-17 20:34:33 +0300

    Unit tests for calculate average method

---
+++ SummerPractice.Application.Tests/NumberUtilitiesTests.cs
@@ -0,0 +1,68 @@
+namespace SummerPractice.Application.Tests
+{
+    using FluentAssertions;
+
+    public class NumberUtilitiesTests
+    {
+        private readonly NumberUtilities _numberUtilities = new();
+
+        [Fact]
+        public void Should_CalculateAverage_When_ValidCommaSeparatedStringIsPassed()
+        {
+            // Arrange
+            var numbers = "1,2,3,4,5";
+
+            // Act
+            var result = this._numberUtilities.CalculateAverage(numbers);
+
+            // Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Should_CalculateAverage_When_StringWithExtraSpacesIsPassed()
+        {
+            // Arrange
+            var numbers = " 1, 2 , 3 , 4, 5 ";
+
+            // Act
+            var result = this._numberUtilities.CalculateAverage(numbers);
+
+            // Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Should_ThrowException_When_EmptyStringIsPassed()
+        {
+            // Arrange
+            var numbers = "";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => this._numberUtilities.CalculateAverage(numbers));
+        }
+
+        [Fact]
+        public void Should_CalculateAverageAndIgnoreInvalidIntegers_When_StringContainsBothValidAndInvalidData()
+        {
+            // Arrange
+            var numbers = "1, 2, three, 4, 5";
+
+            // Act
+            var result = this._numberUtilities.CalculateAverage(numbers);
+
+            // Assert
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Should_ThrowException_When_NoValidIntegersFound()
+        {
+            // Arrange
+            var numbers = "one, two, three";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => this._numberUtilities.CalculateAverage(numbers));
+        }
+    }
+}
